bc69d3c82eb23b7ab3f97dd67a9c8c7b78d53649
Jordan Uyeki <36560901+juyeki@users.noreply.github.com>
2019-11-13 13:56:49 -0800

    Add files via upload

--- R/bar_plot_update_manual.R
+++ R/bar_plot_update_manual.R
@@ -13,14 +13,15 @@
 
 bar_plot_update_manual <- function(a, met, Title, x, y, axis.text.x, scales, type = NULL, num_cond=NULL,index = NULL)
 {
+  color = grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
+  col<-c("turquoise","red","plum4","steelblue1","red4","springgreen2","slateblue2","sienna1","darkgreen","khaki1","lightpink1","navy","olivedrab1",
+         "orangered","darkslateblue","lightseagreen","magenta2","royalblue","yellowgreen","lightsalmon","cyan","maroon1","indianred3","mediumseagreen",
+         "slateblue3","hotpink","lemonchiffon1","orangered4","lightcoral","tomato")
+  col[index[[1]]]<-"yellow1"
+  col[index[[2]]]<-"grey45"
+
   if (num_cond > 11)
   {
-    color = grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
-    col<-c("turquoise","red","plum4","steelblue1","red4","springgreen2","slateblue2","sienna1","darkgreen","khaki1","lightpink1","navy","olivedrab1",
-           "orangered","darkslateblue","lightseagreen","magenta2","royalblue","yellowgreen","lightsalmon","cyan","maroon1","indianred3","mediumseagreen",
-           "slateblue3","hotpink","lemonchiffon1","orangered4","lightcoral","tomato")
-    col[index[[1]]]<-"yellow1"
-    col[index[[2]]]<-"grey45"
     a + geom_bar(position="dodge", stat="identity", width=0.9) +
       geom_bar(position="dodge", stat="identity", colour="black", width=0.9) +
       facet_wrap( ~ Name, scales=scales) +
@@ -41,12 +42,6 @@ bar_plot_update_manual <- function(a, met, Title, x, y, axis.text.x, scales, typ
   }
   else if (type == '250k')
   {
-    col<-c("turquoise","red","plum4","steelblue1","red4","springgreen2","slateblue2","sienna1","darkgreen","khaki1","lightpink1","navy","olivedrab1",
-           "orangered","darkslateblue","lightseagreen","magenta2","royalblue","yellowgreen","lightsalmon","cyan","maroon1","indianred3","mediumseagreen",
-           "slateblue3","hotpink","lemonchiffon1","orangered4","lightcoral","tomato")
-    col[index[[1]]]<-"yellow1"
-    col[index[[2]]]<-"grey45"
-
     a + geom_bar(position="dodge", stat="identity", width=0.9) +
       geom_bar(position="dodge", stat="identity", colour="black", width=0.9) +
       facet_wrap( ~ Name, scales=scales) +
@@ -68,12 +63,6 @@ bar_plot_update_manual <- function(a, met, Title, x, y, axis.text.x, scales, typ
   }
   else
   {
-    col<-c("turquoise","red","plum4","steelblue1","red4","springgreen2","slateblue2","sienna1","darkgreen","khaki1","lightpink1","navy","olivedrab1",
-           "orangered","darkslateblue","lightseagreen","magenta2","royalblue","yellowgreen","lightsalmon","cyan","maroon1","indianred3","mediumseagreen",
-           "slateblue3","hotpink","lemonchiffon1","orangered4","lightcoral","tomato")
-    col[index[[1]]]<-"yellow1"
-    col[index[[2]]]<-"grey45"
-
     a + geom_bar(position="dodge", stat="identity", width=0.9) +
       geom_bar(position="dodge", stat="identity", colour="black", width=0.9) +
       facet_wrap( ~ Name, scales=scales) +
