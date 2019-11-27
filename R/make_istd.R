1594f62865d0091db78069333d56d117d8171cc0
Jordan Uyeki <36560901+juyeki@users.noreply.github.com>
2019-11-27 15:29:50 -0800

    Add files via upload

--- R/make_istd.R
+++ R/make_istd.R
@@ -11,32 +11,48 @@
 #' @export
 #'
 
-make_istd <- function(data)
+make_istd <- function(data, remove)
 {
-  #background subtraction
-  if (sum(grepl('blank|water|buffer', names(data))) > 0) {
-    blank <- data[,grep('blank|water|buffer', names(data))]
-    data <- data[,-grep('blank|water|buffer', names(data))]
-    blank[is.na(blank)] <- 0
-    print("background subtraction")
+  if(remove)
+  {
+    #background subtraction
+    if (sum(grepl('blank|water|buffer', names(data))) > 0) {
+      blank <- data[,grep('blank|water|buffer', names(data))]
+      data <- data[,-grep('blank|water|buffer', names(data))]
+      blank[is.na(blank)] <- 0
+      print("background subtraction")
+    }
+
+    if (sum(grepl('500k_cells|50[Kk]|100[Kk]|250[Kk]|X500[Kk]|200[Kk]', names(data))) > 0) {                          ##collect all standard samples and remove from regular samples
+      Standard <- data[,c(1:3, grep('500k_cells|50[Kk]|100[Kk]|250[Kk]|X500[Kk]|200[Kk]', names(data)))]
+      data <- data[,-grep('500k_cells|50[Kk]|100[Kk]|250[Kk]|X500[Kk]|200[Kk]', names(data))]
+      print("removing standards from sample data")
+    }
   }
 
-  if (sum(grepl('500k_cells|50[Kk]|100[Kk]|250[Kk]|X500[Kk]|200[Kk]', names(data))) > 0) {                          ##collect all standard samples and remove from regular samples
-    Standard <- data[,c(1:3, grep('500k_cells|50[Kk]|100[Kk]|250[Kk]|X500[Kk]|200[Kk]', names(data)))]
-    data <- data[,-grep('500k_cells|50[Kk]|100[Kk]|250[Kk]|X500[Kk]|200[Kk]', names(data))]
-    print("removing standards from sample data")
+  if(remove)
+  {
+    Std <- filter(data, grepl('Std', data$Iso)) %>%
+      dplyr::select(-Iso) %>%
+      gather(Sample, Value, -Used_ID) %>%
+      filter(!(grepl('500k_cells|50[Kk]|100[Kk]|250[Kk]|X500[Kk]', Sample))) %>%
+      group_by(Used_ID) %>%
+      mutate(Av = mean(Value, na.rm=T),
+             Std = sd(Value, na.rm=T),
+             CV = Std / Av,
+             ID = paste0(gsub(' Std','',Used_ID), ' ',round(CV*100,0),'%')) %>%
+      ungroup()
+  } else{
+    Std <- filter(data, grepl('Std', data$Iso)) %>%
+      dplyr::select(-Iso) %>%
+      gather(Sample, Value, -Used_ID) %>%
+      group_by(Used_ID) %>%
+      mutate(Av = mean(Value, na.rm=T),
+             Std = sd(Value, na.rm=T),
+             CV = Std / Av,
+             ID = paste0(gsub(' Std','',Used_ID), ' ',round(CV*100,0),'%')) %>%
+      ungroup()
   }
-
-  Std <- filter(data, grepl('Std', data$Iso)) %>%
-    dplyr::select(-Iso) %>%
-    gather(Sample, Value, -Used_ID) %>%
-    filter(!(grepl('500k_cells|50[Kk]|100[Kk]|250[Kk]|X500[Kk]', Sample))) %>%
-    group_by(Used_ID) %>%
-    mutate(Av = mean(Value, na.rm=T),
-           Std = sd(Value, na.rm=T),
-           CV = Std / Av,
-           ID = paste0(gsub(' Std','',Used_ID), ' ',round(CV*100,0),'%')) %>%
-    ungroup()
 
   result <- list()
   result$data <- data
