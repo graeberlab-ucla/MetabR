4896765cc36e6fc7260ca4f61b6a2f9b1fedb74f
Jordan Uyeki <36560901+juyeki@users.noreply.github.com>
2020-01-15 13:24:40 -0800

    Add files via upload

---
+++ R/glycolysis.R
@@ -0,0 +1,12 @@
+#' Glycolysis pathway defined metabolites.
+#'
+#' @docType data
+#'
+#' @usage data(glycolysis)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+
+"glycolysis"
