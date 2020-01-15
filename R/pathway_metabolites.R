108d1f524af84e2e2aa6d75dcaba3dccf5784d6b
Jordan Uyeki <36560901+juyeki@users.noreply.github.com>
2020-01-15 13:40:50 -0800

    Add files via upload

---
+++ R/pathway_metabolites.R
@@ -0,0 +1,12 @@
+#' File containing a vector of pathway defined metabolites.
+#'
+#' @docType data
+#'
+#' @usage data(pathway_metabolites)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+
+"pathway_metabolites"
