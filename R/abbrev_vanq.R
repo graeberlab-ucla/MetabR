6a97992accb2dd32386eec5ce90d53b976b4d4fc
Jordan Uyeki <36560901+juyeki@users.noreply.github.com>
2020-01-15 14:34:23 -0800

    Add files via upload

---
+++ R/abbrev_vanq.R
@@ -0,0 +1,12 @@
+#' File containing metabolite information.
+#'
+#' Contains metabolite information such as ID names, abbreviations, KEGG.ID, number of carbons, and Pathways.
+#'
+#' @docType data
+#'
+#' @usage data(abbrev_vanq)
+#'
+#' @keywords datasets
+#'
+
+"abbrev_vanq"
