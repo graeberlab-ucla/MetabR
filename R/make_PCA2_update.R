984b83725871aaa1149e71443af398989ac85b4f
Jordan Uyeki <36560901+juyeki@users.noreply.github.com>
2020-02-03 16:44:57 -0800

    Add files via upload

--- R/make_PCA2_update.R
+++ R/make_PCA2_update.R
@@ -6,7 +6,7 @@
 #'
 #' @return A pdf file with a scree plot, a pair plot with the first five PCs, and a PCA plot with PCs a and b.
 #'
-#' @examples update_info(info_dir = "/Users/juyeki/lab_folder")
+#' @examples make_PCA2_update(matrix = RelA)
 #'
 #' @export
 #'
