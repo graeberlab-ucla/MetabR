a7dd492eb0cd8a7f4173fef40a23a703471943f6
Jordan Uyeki <36560901+juyeki@users.noreply.github.com>
2020-02-13 13:49:22 -0800

    Add files via upload

---
+++ R/anno.R
@@ -0,0 +1,12 @@
+#' File containing metabolite information.
+#'
+#' Contains metabolite information with full and abbreviated names for raw data table outputs.
+#'
+#' @docType data
+#'
+#' @usage data(Anno)
+#'
+#' @keywords datasets
+#'
+
+"anno"
