daa7230f4df4282761546f390c553e8a92a4cb1c
Jordan Uyeki <36560901+juyeki@users.noreply.github.com>
2020-03-02 16:28:27 -0800

    Update bar_plot_update_manual.R

--- R/bar_plot_update_manual.R
+++ R/bar_plot_update_manual.R
@@ -11,13 +11,11 @@
 #' @export
 #'
 
-bar_plot_update_manual <- function(a, met, Title, x, y, axis.text.x, scales, type = NULL, num_cond=NULL,index = NULL)
+bar_plot_update_manual <- function(a, met, Title, x, y, axis.text.x, scales, type = NULL, num_cond=NULL,index=NULL)
 {
-  color = grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
   col<-c("turquoise","red","plum4","steelblue1","red4","springgreen2","slateblue2","sienna1","darkgreen","lightpink1","navy","olivedrab1",
          "orangered","darkslateblue","lightseagreen","magenta2","royalblue","yellowgreen","lightsalmon","cyan","maroon1","indianred3","mediumseagreen",
          "slateblue3","hotpink","lemonchiffon1","orangered4","lightcoral","tomato")
-
   if(!is.null(index))
   {
     j  <- 1
@@ -43,11 +41,10 @@ bar_plot_update_manual <- function(a, met, Title, x, y, axis.text.x, scales, typ
     }
     col <- res
   }
-
-
+  
   if (num_cond > 11)
   {
-    a + geom_bar(position="dodge", stat="identity", width=0.9) +
+    a + geom_bar(position="dodge", stat="identity", width=0.9)  +
       geom_bar(position="dodge", stat="identity", colour="black", width=0.9) +
       facet_wrap( ~ Name, scales=scales) +
       theme_bw() +
@@ -56,18 +53,21 @@ bar_plot_update_manual <- function(a, met, Title, x, y, axis.text.x, scales, typ
         plot.title=element_text(size=20, face="bold", vjust=2),         #sets title properties
         axis.title=element_text(size=16, lineheight=20, face="bold"),   #sets theme for axis font
         axis.text=element_text(size=11, face="bold"),
-        #axis.text.x=element_blank(),
         axis.text.x=axis.text.x,
         legend.title=element_text(face="bold", size=12),
         legend.text=element_text(face="bold",size=12),                  #sets legend text
         strip.text=element_text(face="bold", size=15),           #sets theme for title in facets
         panel.grid.major=element_blank()) +
       geom_errorbar(aes(ymin=RelAmounts_Ave, ymax=RelAmounts_Ave+RelAmounts_Std), position=position_dodge(0.9), width=.2)+
-      scale_fill_manual(values = col)
+      scale_fill_manual(values = col)+ geom_text(aes(label=under_50_percent), position=position_dodge(width=0.9), vjust=-0.25)+
+      geom_bar(position="dodge", stat="identity", colour="black", width=0.9)
   }
   else if (type == '250k')
   {
     a + geom_bar(position="dodge", stat="identity", width=0.9) +
+      geom_bar(aes(linetype=under_50_percent,color = under_50_percent, size=under_50_percent),position="dodge", stat="identity", width=0.9) +
+      guides(linetype=FALSE)+
+      scale_size_manual(values=c(0.3,0.8), guide = F) + scale_colour_manual(values = c("black", "gray29"), guide = F) +
       geom_bar(position="dodge", stat="identity", colour="black", width=0.9) +
       facet_wrap( ~ Name, scales=scales) +
       theme_bw() +
@@ -76,36 +76,34 @@ bar_plot_update_manual <- function(a, met, Title, x, y, axis.text.x, scales, typ
         plot.title=element_text(size=20, face="bold", vjust=2),         #sets title properties
         axis.title=element_text(size=16, lineheight=20, face="bold"),   #sets theme for axis font
         axis.text=element_text(size=11, face="bold"),
-        #axis.text.x=element_blank(),
         axis.text.x=axis.text.x,
         legend.title=element_text(face="bold", size=12),
         legend.text=element_text(face="bold",size=12),                  #sets legend text
         strip.text=element_text(face="bold", size=15),           #sets theme for title in facets
         panel.grid.major=element_blank()) +
       geom_errorbar(aes(ymin=RelAmounts_Ave, ymax=RelAmounts_Ave+RelAmounts_Std), position=position_dodge(0.9), width=.2)+
-      #scale_fill_brewer(palette = "Spectral")
       scale_fill_manual(values = col)
   }
   else
   {
     a + geom_bar(position="dodge", stat="identity", width=0.9) +
-      geom_bar(position="dodge", stat="identity", colour="black", width=0.9) +
+      geom_bar(aes(linetype=under_50_percent,color = under_50_percent, size=under_50_percent),position="dodge", stat="identity", width=0.9) +
+      guides(linetype=FALSE)+
+      scale_size_manual(values=c(0.3,0.8), guide = F) + scale_colour_manual(values = c("black", "gray29"), guide = F) +
       facet_wrap( ~ Name, scales=scales) +
-      theme_bw() +
+      theme_bw() +scale_linetype_manual(values=c("solid","58"))+
       labs(x=x, y=y, title=Title, fill=element_blank()) +
       theme(
         plot.title=element_text(size=20, face="bold", vjust=2),         #sets title properties
         axis.title=element_text(size=16, lineheight=20, face="bold"),   #sets theme for axis font
         axis.text=element_text(size=11, face="bold"),
-        #axis.text.x=element_blank(),
         axis.text.x=axis.text.x,
         legend.title=element_text(face="bold", size=12),
         legend.text=element_text(face="bold",size=12),                  #sets legend text
         strip.text=element_text(face="bold", size=15),           #sets theme for title in facets
         panel.grid.major=element_blank()) +
       geom_errorbar(aes(ymin=RelAmounts_Ave, ymax=RelAmounts_Ave+RelAmounts_Std), position=position_dodge(0.9), width=.2)+
-      #scale_fill_brewer(palette = "Set1")
       scale_fill_manual(values = col)
   }
-
+  
 }
