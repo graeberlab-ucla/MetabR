63e96928f8601efc906ee6353ce11289d8f2f507
Jordan Uyeki <36560901+juyeki@users.noreply.github.com>
2020-05-22 13:33:21 -0700

    Add files via upload

---
+++ R/molecule_C.R
@@ -0,0 +1,12 @@
+#' File containing metabolite information.
+#'
+#' Contains molecule information for use with IsoCorrectoR on Carbon Labeled experiments.
+#'
+#' @docType data
+#'
+#' @usage data(molecule_C)
+#'
+#' @keywords datasets
+#'
+
+"molecule_C"
