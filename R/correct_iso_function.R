e98379a60f1bcc4f07e3baa545ccdabbdabf7d62
Daniel Tan <44482565+danjst@users.noreply.github.com>
2020-05-26 16:39:30 -0700

    Update correct_iso_function.R

--- R/correct_iso_function.R
+++ R/correct_iso_function.R
@@ -11,17 +11,6 @@
 #' @export
 #'
 
-library(qdap)
-library(tidyverse)
-library(dplyr)
-library(data.table)
-library(tidyr)
-library(IsoCorrectoR)
-library(writexl)
-library(stringr)
-library(readr)
-library(plyr)
-
 
 correct_iso<-function(df,label,correction){
   df_name<-paste(deparse(substitute(df)))
