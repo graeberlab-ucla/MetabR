0454dd699e298331bfaf4f55be3746f201c33476
thomglen <tgraeber@mednet.ucla.edu>
2020-11-10 23:42:45 -0800

    update Abbrev

---
+++ R/Abbrev.R
@@ -0,0 +1,13 @@
+#' File containing metabolite information.
+#'
+#' Contains metabolite information such as ID names, abbreviations, KEGG.ID, number of carbons, and Pathways.
+#' Corresponds to data collected using ICS chromatography
+#'
+#' @docType data
+#'
+#' @usage data(Abbrev)
+#'
+#' @keywords datasets
+#'
+
+"Abbrev"
