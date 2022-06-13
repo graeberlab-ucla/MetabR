b5e438162211afc6470a4a8f8f0140750cd67768
Ashvath Balgovind <ashvathbalgovind@gmail.com>
2022-06-13 15:25:41 -0700

    updated pools

--- R/bar_plot_update_manual.R
+++ R/bar_plot_update_manual.R
@@ -35,6 +35,7 @@ bar_plot_update_manual <- function(a, met, Title, x, y, axis.text.x, scales, typ
         res <- c(res, "grey45")
       else if (i %in% index[[4]]){
         res <- c(res, "darkorange1")
+      }
       else if(i %in% index[[3]])
       {
         res <- c(res, extra_qc[k])
