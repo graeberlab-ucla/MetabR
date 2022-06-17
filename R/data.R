7771a7571fdb7a9c34d212bc1c5824405e60dc25
1014violin <1014violin@gmail.com>
2022-06-17 11:53:14 -0700

    added data.R

---
+++ R/data.R
@@ -0,0 +1,142 @@
+#' A character vector of amino acids.
+#'
+#'@format A character vector of length 22.
+"AA"
+
+#' A data frame with metabolite naming conventions used in this package.
+#'
+#'@format A data frame with seven variables and 204 entries
+"Abbrev"
+
+#' A character vector of adenine-related compounds.
+#'
+#'@format A character vector of length 13.
+"Adenine"
+
+#' A character vector of co-enzyme A-related compounds.
+#'
+#'@format A character vector of length 9.
+"CoAs"
+
+#' A data frame with compound information for 15710 from the KEGG database as well as 41500 HMDB
+#' compounds. The compounds are overlapping and also include calculated mass-to-charge values for 111
+#' salt clusters.
+#'
+#'@format A data frame of 6294970 rows and 12 variables.
+"CompMZ"
+
+#' A character vector of currency metabolites.
+#'
+#'@format A character vector of length 16.
+"Curr"
+
+#' A character vector of cysteine metabolism compounds.
+#'
+#'@format A character vector of length 11.
+"Cys"
+
+#' A character vector of cytosine-related compounds.
+#'
+#'@format A character vector of length 9.
+"Cytosine"
+
+#' A character vector of fatty acid-related compounds.
+#'
+#'@format A character vector of length 11.
+"FA"
+
+#' A character vector of fructose metabolism compounds.
+#'
+#'@format A character vector of length 9.
+"Fru"
+
+#' A character vector of guanine metabolism compounds.
+#'
+#'@format A character vector of length 8.
+"Guanine"
+
+#' A character vector of hexose metabolism compounds.
+#'
+#'@format A character vector of length 8.
+"Hex"
+
+#' A data frame with 514 KEGG pathways and their corresponding IDs.
+#'
+#'@format A data frame with 514 rows and 2 variables.
+"KEGG.pathway"
+
+#' A character vector of neurotransmitter compounds.
+#'
+#'@format A character vector of length 5.
+"Neurotrans"
+
+#' A character vector of pentose phosphate metabolism compounds.
+#'
+#'@format A character vector of length 14.
+"PPP"
+
+#' A character vector of compounds to be used as MID inputs for INCA.
+#'
+#'@format A character vector of length 12.
+"QE"
+
+#' A character vector of Tricarboxylic acid (TCA) metabolism compounds.
+#'
+#'@format A character vector of length 14.
+"TCA"
+
+#' A character vector of uracil metabolism compounds.
+#'
+#'@format A character vector of length 7.
+"Uracil"
+
+#' A character vector of commonly used colors.
+#'
+#'@format A character vector of length 18.
+"colors"
+
+#' A data frame of 3443 KEGG metabolites and their association with 319 KEGG pathways.
+#'
+#'@format A data frame of 5860 rows and 5 variables.
+"compound.pathway"
+
+#' A data frame of 18042 KEGG compounds.
+#'
+#'@format A data frame of 18042 rows and 4 variables.
+"compounds"
+
+#' A data frame of genes associated with 319 KEGG pathways. This data frame is useful for
+#' gene set enrichment analysis if reshaped.
+#'
+#'@format A data frame of 25580 rows and 8 variables.
+"gene.pathway"
+
+#' A character vector of glycolysis compounds.
+#'
+#'@format A character vector of length 26.
+"glycolysis"
+
+#' A character vector of colors used in heatmaps.
+#'
+#'@format A character vector of length 3.
+"normal"
+
+#' A list with all information for 319 KEGG pathways.
+#'
+#'@format A character vector of length 7.
+"paths"
+
+#' A character vector of purine metabolism compounds.
+#'
+#'@format A character vector of length 7.
+"purine"
+
+#' A character vector of pyrimidine metabolism compounds.
+#'
+#'@format A character vector of length 7.
+"pyrimidine"
+
+#' A character vector of thymine metabolism compounds.
+#'
+#'@format A character vector of length 5.
+"Thymine"
