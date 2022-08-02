ec2147eb09cb66104e256b3bf4fc9f1ea3d3105c
1014violin <1014violin@gmail.com>
2022-08-02 16:34:12 -0700

    added Custom metab code to script

--- R/Maven_MID_Plot.R
+++ R/Maven_MID_Plot.R
@@ -1,13 +1,13 @@
 #' Maven_MID_plot
 #'
-#' @param metabolites 
-#' @param df 
-#' @param repeats 
+#' @param metabolites
+#' @param df
+#' @param repeats
 #' @param n number of conditions
-#' @param type 
-#' @param index 
-#' @param title_type 
-#' @param only_M0 
+#' @param type
+#' @param index
+#' @param title_type
+#' @param only_M0
 #'
 #' @return
 #' @export
@@ -96,18 +96,23 @@ Maven_MID_plot <- function(metabolites, df, repeats, n, type, index = NULL, titl
     metabolites <- Neurotrans
     ending <- "Neurotransmitter levels"
   }
+  else if (metabolites=='Custom')
+  {
+    metabolites = Custom
+    ending <- 'Custom metabolites'
+  }
   else ending = ""
   if (sum(grepl("MID", names(df))) >= 1) {
     met = subset(df, Name %in% metabolites)
     stopifnot(length(unique(met$Name)) >= 1)
-    met <- mutate(met, Iso = paste(Iso, Sig, sep = "\n"), 
-                  Sig = "") 
-    met <- met %>% 
+    met <- mutate(met, Iso = paste(Iso, Sig, sep = "\n"),
+                  Sig = "")
+    met <- met %>%
       mutate(Iso = factor(Iso, levels = sort(unique(Iso))))
     Title = paste0("Isotopologue distribution of ", ending, " (Corrected for natural abundance)")
     x <- "Isotopologue"
     y <- "% Labeled"
-    a <- ggplot(met, aes(Iso, RelAmounts_Ave, group = Condition, 
+    a <- ggplot(met, aes(Iso, RelAmounts_Ave, group = Condition,
                          fill = Condition, label = Sig))
     temp <- a$data
     # if (sum(unique(temp$Name) %in% only_M0) > 0){
@@ -116,23 +121,23 @@ Maven_MID_plot <- function(metabolites, df, repeats, n, type, index = NULL, titl
     a$data <- temp
     axis.text.x = element_text(size = 7, face = "bold", angle = 90)
     if(dim(temp)[1]==0) return()
-    bar_plot_update_manual(a, met, Title, x, y, axis.text.x, 
+    bar_plot_update_manual(a, met, Title, x, y, axis.text.x,
                            scales = "free", num_cond = n, type = "tf")
   }
   else if (sum(grepl("Exp", names(df))) >= 1) {
     if (type == "tf") {
       met = subset(df, Name %in% metabolites)
       stopifnot(length(unique(met$Name)) >= 1)
-      met <- mutate(met, Name = paste(Name, Sig, sep = " "), 
+      met <- mutate(met, Name = paste(Name, Sig, sep = " "),
                     Sig = "")
       Title = paste0("Relative amounts of ", ending, " (not corrected for blank values)")
       x = ""
       y = "Relative Amounts"
-      a <- ggplot(met, aes(Condition, RelAmounts_Ave, 
+      a <- ggplot(met, aes(Condition, RelAmounts_Ave,
                            group = Condition, fill = Condition, label = Sig))
       axis.text.x = element_blank()
-      bar_plot_update_manual(a, met, Title, x, y, axis.text.x, 
-                             scales = "free", num_cond = n, type = type, 
+      bar_plot_update_manual(a, met, Title, x, y, axis.text.x,
+                             scales = "free", num_cond = n, type = type,
                              index)
     }
     else {
@@ -141,11 +146,11 @@ Maven_MID_plot <- function(metabolites, df, repeats, n, type, index = NULL, titl
       Title = paste0("Relative amounts of ", ending)
       x = ""
       y = "Relative Amounts"
-      a <- ggplot(met, aes(Condition, RelAmounts_Ave, 
+      a <- ggplot(met, aes(Condition, RelAmounts_Ave,
                            group = Condition, fill = Condition, label = Name))
       axis.text.x = element_blank()
-      bar_plot_update_manual(a, met, Title, x, y, axis.text.x, 
-                             scales = "free", num_cond = n, type = type, 
+      bar_plot_update_manual(a, met, Title, x, y, axis.text.x,
+                             scales = "free", num_cond = n, type = type,
                              index)
     }
   }
@@ -153,41 +158,41 @@ Maven_MID_plot <- function(metabolites, df, repeats, n, type, index = NULL, titl
     met <- subset(df, Name %in% metabolites)
     stopifnot(length(unique(met$Name)) >= 1)
     met$Names <- met$Name
-    met <- mutate(met, Name = paste(Name, Sig, sep = " "), 
+    met <- mutate(met, Name = paste(Name, Sig, sep = " "),
                   Sig = "")
     Title <- paste0("Percent labeled in ", ending)
     x <- ""
     y <- "% Labeled"
-    a <- ggplot(met, aes(Condition, RelAmounts_Ave, group = Condition, 
+    a <- ggplot(met, aes(Condition, RelAmounts_Ave, group = Condition,
                          fill = Condition, label = Sig))
     temp <- a$data
-    # if (sum(unique(temp$Names) %in% only_M0) > 0) 
+    # if (sum(unique(temp$Names) %in% only_M0) > 0)
     #   temp <- temp[-which((temp$Names %in% only_M0)),]
     # }
     a$data <- temp
     axis.text.x = element_blank()
-    bar_plot_update_manual(a, met, Title, x, y, axis.text.x, 
+    bar_plot_update_manual(a, met, Title, x, y, axis.text.x,
                            scales = "fixed", num_cond = n, type = type, index)
   }
   else if (sum(grepl("FC", names(df))) >= 1) {
     met <- subset(df, Name %in% metabolites)
     stopifnot(length(unique(met$Name)) >= 1)
     met$Names <- met$Name
-    met <- mutate(met, Name = paste(Name, Sig, sep = " "), 
+    met <- mutate(met, Name = paste(Name, Sig, sep = " "),
                   Sig = "")
     Title <- paste0("Fractional Contribution to ", ending)
     x <- ""
     y <- "% Fractional Contribution"
-    a <- ggplot(met, aes(Condition, RelAmounts_Ave, group = Condition, 
+    a <- ggplot(met, aes(Condition, RelAmounts_Ave, group = Condition,
                          fill = Condition, label = Sig))
     temp <- a$data
-    # if (sum(unique(temp$Names) %in% only_M0) > 0) 
+    # if (sum(unique(temp$Names) %in% only_M0) > 0)
     #   temp <- temp[-which((temp$Names %in% only_M0)), ]
     # }
     a$data <- temp
     axis.text.x = element_blank()
     if(dim(temp)[1]==0) return()
-    bar_plot_update_manual(a, met, Title, x, y, axis.text.x, 
+    bar_plot_update_manual(a, met, Title, x, y, axis.text.x,
                            scales = "fixed", num_cond = n, type = type, index)
   }
 }
