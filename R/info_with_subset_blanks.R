cf5c03ddb4cf3ae6e7fc12276b1273864c1f0d68
1014violin <1014violin@gmail.com>
2022-10-27 10:38:38 -0700

    fixed no output problem

--- R/info_with_subset_blanks.R
+++ R/info_with_subset_blanks.R
@@ -1,16 +1,16 @@
 #' info_with_subset_blanks
 #'
-#' @param infoA data frame that has columns Sample, Run.Order, Condition, etc. 
+#' @param infoA data frame that has columns Sample, Run.Order, Condition, etc.
 #' @description This function takes a subset sample info sheet and only keeps the two QC-Blanks that were run directly before/ after the samples.
 #' The purpose of this function is to prevent unwanted filtering before calling filter.R,
-#'  that can arise when we include QC-blank values that are not associated with this subset of samples. 
+#'  that can arise when we include QC-blank values that are not associated with this subset of samples.
 #' @return An info sheet that only contains the two QC-blanks that pertain to this subset.
 #' @export
 #'
 #' @examples
 info_with_subset_blanks<-function(info){
-#only use on subsets 
-samples<-info[which(!grepl("QC|[Pp]ool", info$Sample)),] %>% 
+#only use on subsets
+samples<-info[which(!grepl("QC|[Pp]ool", info$Sample)),] %>%
   arrange(Run.Order)
 
 first_run_sample<-samples[1,]
@@ -29,9 +29,11 @@ lastqc<-min(as.numeric(lastqc_diff$V2))
 blank2<-lastqc_diff%>%filter(V2==as.character(lastqc))
 blank2<-blank2$V1 #QC-Blank run right after samples
 
+if(dim(qc_blanks)[1]>2){
 qc_to_remove<-qc_blanks[which(!grepl(blank1, qc_blanks$Sample) & !grepl(blank2, qc_blanks$Sample)),]
 m_ind<-match(qc_to_remove$Sample, info$Sample)
 info<-info[-m_ind,]
+}
 
 return(info)
 }
