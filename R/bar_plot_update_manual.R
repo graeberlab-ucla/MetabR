5096a106611dae906af614e911e14c15e5dc167f
1014violin <1014violin@gmail.com>
2023-01-26 15:53:13 -0800

    got rid of color sienna1 as it looks too similar to qc pool orange color

--- R/bar_plot_update_manual.R
+++ R/bar_plot_update_manual.R
@@ -18,7 +18,7 @@
 #'
 bar_plot_update_manual <- function(a, met, Title, x, y, axis.text.x, scales, type = NULL, num_cond=NULL,index=NULL)
 {
-  col<-c("turquoise","red","plum4","steelblue1","red4","springgreen2","slateblue2","sienna1","darkgreen","lightpink1","navy","olivedrab1",
+  col<-c("turquoise","red","plum4","steelblue1","red4","springgreen2","slateblue2","darkgreen","lightpink1","navy","olivedrab1",
          "orangered","darkslateblue","lightseagreen","magenta2","royalblue","yellowgreen","lightsalmon","cyan","maroon1","indianred3","mediumseagreen",
          "slateblue3","hotpink","lemonchiffon1","orangered4","lightcoral","tomato")
   if(!is.null(index))
