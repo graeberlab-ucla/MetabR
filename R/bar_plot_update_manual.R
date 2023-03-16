9d36ba51ca905930fb01ec570745a5797548072d
1014violin <1014violin@gmail.com>
2023-03-16 15:24:02 -0700

    color change in bar update plot manual

--- R/bar_plot_update_manual.R
+++ R/bar_plot_update_manual.R
@@ -18,7 +18,7 @@
 #'
 bar_plot_update_manual <- function(a, met, Title, x, y, axis.text.x, scales, type = NULL, num_cond=NULL,index=NULL)
 {
-  if(exists(colors)){
+  if(exists('colors')){
     col<-colors
   }else{
   col<-c("turquoise","red","plum4","steelblue1","red4","springgreen2","slateblue2","darkgreen","lightpink1","navy","olivedrab1",
