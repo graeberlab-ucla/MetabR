ce315c7608867a0b683d652e5f2eb3f36b693435
1014violin <1014violin@gmail.com>
2024-01-26 13:42:10 -0800

    changed way of defining other samples category.

--- R/update_info_maven.R
+++ R/update_info_maven.R
@@ -31,9 +31,6 @@ run_order$Run.Order <- rownames(run_order)
 #make info$sample namings match run_order$Sample
 #Alexzandra added 1/25/22 - lists blanks before 250ks
 samples <- as.vector(run_order$samples)
-#other samples such as "ISTD" or STD or ImP that have different concentrations - usually standards
-other <- samples[!stringr::str_detect(samples, "^[A-Z]{2}[\\-\\.]")]
-#other <- other[other %in% info$Sample] #may be necessary if the ordering is incorrect of the "other" samples
 
 qc_blank <- samples[grepl("blank", samples, ignore.case = T) & !grepl("PB", samples, ignore.case = T)]
 qc_blank <- qc_blank[order(qc_blank)]
@@ -45,10 +42,17 @@ qc_pool <- samples[grepl("pool|Pool", samples, ignore.case = T)]
 qc_pool <- qc_pool[order(qc_pool)]
 
 #processing blanks
-qc_other<-samples[grepl("QC|PB", samples, ignore.case = T)][!grepl("pool|Pool|250|blank", samples[grepl("QC", samples, ignore.case = T)], ignore.case = T)]
+qc_other<-samples[grepl("QC&|PB", samples, ignore.case = T)]
 qc_other <- qc_other[order(qc_other)]
 
-samples <- samples[!(samples %in% qc_blank) & !(samples %in% qc_250k) &!(samples %in% qc_pool) &!(samples %in% qc_other) &!(samples %in% other)]
+remaining_samples <- samples[!(samples %in% qc_blank) & !(samples %in% qc_250k) &!(samples %in% qc_pool) &!(samples %in% qc_other)]
+
+#other samples such as "ISTD" or STD or ImP that have different concentrations - usually standards
+other<-remaining_samples[which(!grepl("^[A-Z]{2}[-.][0123456789]{3}", remaining_samples))]
+#other <- other[other %in% info$Sample] #may be necessary if the ordering is incorrect of the "other" samples
+
+samples<-remaining_samples[which(grepl("^[A-Z]{2}[-.][0123456789]{3}", remaining_samples))]
+
 samples <- samples[order(samples)]
 sample.length <- length(samples)
 samples <- c(samples, other)
