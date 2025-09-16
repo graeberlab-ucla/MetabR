27baab74d36aa32277d5571f471718f74de2729b
1014violin <1014violin@gmail.com>
2025-09-15 18:30:15 -0700

    made make_matrix more foolproof to handle edge cases

--- R/make_matrix.R
+++ R/make_matrix.R
@@ -2,7 +2,7 @@
 #'
 #' @author Daniel Braas
 #' @param MS_data The data frame to be used for the matrix. Can be relative amounts, fractional contribution or percent labeled.
-#' @param Type Which type of data is used. Right now this is not a used variable, but will become important if I add something for isotopologues. 
+#' @param Type Which type of data is used. Right now this is not a used variable, but will become important if I add something for isotopologues.
 #' @param anova A cutoff value: usually a p-value of some sort
 #'
 #' @return
@@ -22,16 +22,18 @@ make_matrix <- function(MS_data, Type, anova=0.05){
       mutate(Condition_Exp=factor(Condition_Exp, levels=unique(Condition_Exp))) %>%
       unite(Name_Iso, c(Name, Iso), sep='_') %>%
       spread(Condition_Exp, Value)
-    #data9 <- data9[,!(colSums(data9[2:length(data9)], na.rm=T)==0)]
-    data9 <- data9[!(rowSums(data9[2:length(data9)], na.rm=T)==0),]
-    
+
+    if (ncol(data9) <= 1) return(NULL)
+
+    data9 <- data9[!(rowSums(data9[, 2:ncol(data9), drop = FALSE], na.rm = TRUE) == 0), ]
+
   } else {
-    
+
     data9 <- MS_data %>%
       filter(ANOVA <= anova) %>%
       select(Name, Condition, grep('Exp|FC|Labeled', names(MS_data))) %>%
       gather(Exp, Value, -Name, -Condition) %>%
-      arrange(Name, Condition) %>% 
+      arrange(Name, Condition) %>%
       #mutate(Exp = as.numeric(gsub('Exp|FC|Labeled','',.$Exp))) %>%
       group_by(Name) %>%
       mutate(Nr.NA = sum(is.na(Value)),
@@ -44,12 +46,28 @@ make_matrix <- function(MS_data, Type, anova=0.05){
       mutate(Condition_Exp=factor(Condition_Exp, levels=unique(Condition_Exp))) %>%
       spread(Condition_Exp, Value)
   }
-  
+
   data9[is.na(data9)] <- 0
-  data5=as.matrix(data9[2:length(data9)])
+
+  if (ncol(data9) < 2) return(NULL)
+
+  data5 <- as.matrix(data9[, 2:ncol(data9), drop = FALSE])
   rownames(data5) <- data9$Name
-  data5 <- data5[!(rowSums(data5))==0,]
-  data5 <- data5[,!(colSums(data5))==0]
-  
+
+  if (nrow(data5) < 2) return(NULL) # If one or less significant metabolite return NULL
+
+  data5 <- data5[rowSums(data5) != 0, , drop = FALSE]
+
+  cols_to_remove <- which(
+    colSums(data5) == 0 &
+      !(gsub("_Exp", "_", colnames(data5)) %in% info$Sample.Name)
+  )
+
+  if (length(cols_to_remove) > 0) {
+    data5 <- data5[, -cols_to_remove, drop = FALSE]
+  }
+
+  if (ncol(data5) == 0) return(NULL)
+
   return(data5)
 }
